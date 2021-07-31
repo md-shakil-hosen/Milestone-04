e3514d8d394b806cb2293cf10bcc26e2a047a6d7
md-shakil-hosen <shakilinfo91@gmail.com>
2021-07-31 15:38:54 +0600

    Well  I am Happy

--- conversion.js
+++ conversion.js
@@ -5,3 +5,48 @@ var firstName = 'Shakil';
 var lastName = 'Mahmud';
 var fullName = firstName + ' ' + lastName;
 console.log(fullName);
+
+// Converted String Number to Just Number 
+var OnionPrice = '40.44';
+var EggPrice = '41.2';
+var OnionPriceNumber = parseInt(OnionPrice);
+var EggPriceNumber = parseInt(EggPrice);
+console.log(OnionPriceNumber);
+console.log(EggPriceNumber);
+
+// Convert String Number to Floating Number 
+var OnionPrice = '40.44';
+var EggPrice = '41.2';
+var OnionPriceNumber = parseFloat(OnionPrice);
+var EggPriceNumber = parseFloat(EggPrice);
+console.log(OnionPriceNumber);
+console.log(EggPriceNumber);
+
+// Modules 
+var number1 = 17;
+var number2 = 5;
+var modulusNumber = number1 % number2;
+console.log(modulusNumber);
+
+// Object 
+
+var today = new Date();
+console.log(today);
+
+// Special Edition 
+// Floating Point Fixed Number
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var totalNumber = firstNumber + secondNumber;
+console.log(totalNumber);
+
+
+// Fixed Number
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var totalNumber = firstNumber + secondNumber;
+console.log(totalNumber.toFixed(2));
+
+// Completed The Modiul Very Goodly , Great you are very good ,
+//  Your try to more good man from mind 
+//  Then you success . Thanks you very much
