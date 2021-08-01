1c277f038cbed4e0c637a352d7c3f633e6500bed
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-01 07:50:17 +0600

    array

---
+++ array.js
@@ -0,0 +1,19 @@
+// How to declare Array
+// Ex:
+var friendsAge = 20; /* Declare for one person */
+var friendsAge = [11, 13, 15, 17, 19]; /* Variables Declare for more person , It's Array */
+
+// Array 
+var muriChanacurFee = [30, 20, 40, 50, 60,];
+var oddNumber = [1, 3, 5, 7, 9, 11];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var friendsNameList = ["Rakib", "Ashik", "Pias", "Babu", "Abir"];
+
+console.log(oddNumber.length);
+
+// Count Array Elements 
+
+var friendsNameList = ["Rakib", "Ashik", "Pias", "Babu", "Abir"];
+var friendsNameListCount = friendsNameList.length;
+console.log(friendsNameListCount);
+
