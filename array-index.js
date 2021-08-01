10a5e4acb619090c07e0114242079716b049d8d6
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-01 09:41:47 +0600

    Array Index

---
+++ array-index.js
@@ -0,0 +1,14 @@
+// Explain Array Index
+var bookNames = ["Never Stop Learning", "Poor", "Power of Thinking", "Negative Think", "Python Programming"];
+var PoorDadIndex = bookNames.indexOf("Poor"); /**Most Be Used ("") in IndexOF */
+console.log(PoorDadIndex);
+
+// Find IndexNumber in Array
+var numbers = [45, 50, 56, 89, 39, 40, 43, 29];
+var Index = numbers.indexOf(56); /* 56 Index Position is  2  II Explain: 0(45) 1(50) 2(56)*/
+console.log(Index);
+
+// Find Array elements of position 
+var books = ["Data_Structure", "PCB_Design", "Python_Programming"];
+var threeIndex = books.indexOf("Python_Programming"); /* Include name Book , Check Position */
+console.log(threeIndex);
