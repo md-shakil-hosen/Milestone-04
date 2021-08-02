c51ddffdcbc558ef12a9c15bf8918ca7379c52e0
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-02 15:45:47 +0600

    Unlimited For Loop

---
+++ for-loop.js
@@ -0,0 +1,21 @@
+// For Loop
+var i = 0;
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// For Loop Limit Numbers
+var numbers = [45, 25, 258, 258, 266, 25, 23, 25, 286, 63, 33, 56];
+for (var i = 0; i < 12; i++) {
+    var element = numbers[i];
+    console.log(element);
+}
+
+// For Loop Unlimited Number 
+
+var numbers = [45, 25, 258, 258, 266, 25, 23, 25, 286, 63, 33, 56];
+for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    console.log(element);
+
+}
