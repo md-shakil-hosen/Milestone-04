a1e1c2dead112ccb59b6369501b0dbfc18df5c88
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-03 20:54:21 +0600

    Function

---
+++ function.js
@@ -0,0 +1,11 @@
+function StartFan() {
+    console.log('Get Up And Go To  near of Switch');
+    console.log('Then Click the Switch');
+}
+StartFan(); {
+    console.log('Eat breakfast');
+    console.log('Drink Tea');
+}
+StartFan(); {
+    console.log('Come Back to Room');
+}
