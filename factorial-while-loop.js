3c429b1f28ae431b27fd0258b06589257cd3e5b4
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-04 16:58:48 +0600

    Factorial While loop

---
+++ factorial-while-loop.js
@@ -0,0 +1,26 @@
+// // Factorial While Loop
+// function getFactorial(number) {
+//     let Factorial = 1;
+//     let i = 1;
+//     while (i <= number) {
+//         Factorial = Factorial * i;
+//         i++;
+//     }
+//     return Factorial;
+// }
+
+// const MyFactorial = getFactorial(6);
+// console.log(MyFactorial);
+
+// Decrement
+function getFactorial(number) {
+    let Factorial = 1;
+    let i = number;
+    while (i >= 1) {
+        Factorial = Factorial * i;
+        i--;
+    }
+    return Factorial;
+}
+const MyFactorial = getFactorial(3);
+console.log(MyFactorial);
