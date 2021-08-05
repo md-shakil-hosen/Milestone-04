87a2ec203c695b850956f09a2544bde9ebb954d9
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-05 11:38:17 +0600

    Switch

---
+++ switch.js
@@ -0,0 +1,28 @@
+// switch
+var Color = 'Yellow';
+// if (Color = 'Blue') {
+//     console.log('Color is Blue');
+// }
+// else if (Color == 'Red') {
+//     console.log('Color is Blue');
+// }
+// else if (Color == 'Red') {
+//     console.log('Color is Blue');
+
+// }
+// else {
+//     console.log('Color is Black');
+// }
+switch (Color) {
+    case 'Blue':
+        console.log('Color is blue');
+        break;
+    case 'Red':
+        console.log('Color is Red');
+        break;
+    case 'Yellow':
+        console.log('Color is Yellow');
+        break;
+    default:
+        console.log('Color is Black');
+}
